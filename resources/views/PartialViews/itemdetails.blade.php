c743e16b333dbaa7ae4afb8964acfc0199584d8b
ishwordeep <ishwor055@gmail.com>
2022-08-22 07:23:36 +0545

    wip

--- resources/views/PartialViews/itemdetails.blade.php
+++ resources/views/PartialViews/itemdetails.blade.php
@@ -39,5 +39,88 @@
                 Sales Overview
             </h3>
         </div>
+        <div style="height:500px;width:600px;">
+            <canvas id="myChart"></canvas>
+        </div>
     </div>
 </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        debugger;
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                    label: 'Item A',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                },
+                {
+                    label: 'Item B',
+                    backgroundColor: 'rgb(55, 9, 32)',
+                    borderColor: 'rgb(55, 9, 32)',
+                    data: [0, 30, 52, 12, 70, 230, 45],
+                },
+                // {
+                //     label: 'Item C',
+                //     backgroundColor: 'rgb(5, 99, 132)',
+                //     borderColor: 'rgb(5, 99, 132)',
+                //     data: [0, 10, 92, 12, 220, 20, 45],
+                // }
+            ]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                animations: {
+                    tension: {
+                        duration: 2000,
+                        easing: 'linear',
+                        from: 1,
+                        to: 0,
+                        loop: false
+                    }
+                },
+                plugins: {
+                    title: {
+                display: true,
+                text: 'Sales Chart'
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = context.dataset.label || '';
+
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.y !== null) {
+                            label += new Intl.NumberFormat('en-US').format(context.parsed.y);
+                        }
+                        return label;
+                    }
+                }
+            },
+           
+        }
+            }
+        };
+
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
