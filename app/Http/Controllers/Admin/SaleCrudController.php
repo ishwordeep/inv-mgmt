b1d72ed531efda65728079b5b8164372befa786b
ishwordeep <ishwor055@gmail.com>
2022-08-27 05:29:36 +0545

    sales item qty decrease in update

--- app/Http/Controllers/Admin/SaleCrudController.php
+++ app/Http/Controllers/Admin/SaleCrudController.php
@@ -283,6 +283,31 @@ class SaleCrudController extends BaseCrudController
                             'item_total' =>$request->item_amount[$key],
                         ];
                         $here = SaleItem::create($itemArray);
+                        if ($request->status_id === MstSupStatus::APPROVED) {
+                            // dd("hell",$this->user);
+                            $arr = [
+                                'store_id' => $this->user->store_id,
+                                'item_id' => $itemArray['item_id'],
+                                'created_by' => $this->user->id,
+                            ];
+                    
+                    
+                           
+                                $arr['item_qty'] = $itemArray['total_qty'];
+                                $existingItemQty =ItemDetail::where([
+                                    'store_id' => $this->user->store_id,
+                                    'item_id' => $itemArray['item_id'],
+                                ])->first();
+                    
+                                $flag = $existingItemQty ?? false;
+                           
+                            if ($flag) {
+                                $flag->item_qty = $existingItemQty->item_qty-$arr['item_qty'];
+                                $flag->save();
+                            } else {
+                                ItemDetail::create($arr);
+                            }
+                        }
                     }
                 }
                 DB::commit();
